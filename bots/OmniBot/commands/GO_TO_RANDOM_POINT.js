f4097f3548f75d864fb05e76ea9f80978b746150
fikys <1234567daun@gmail.com>
2024-12-22 19:18:21 +0300

    added explore goal

---
+++ bots/OmniBot/commands/GO_TO_RANDOM_POINT.js
@@ -0,0 +1,14 @@
+const {createGoal} = require('../goals.js')
+module.exports = {
+    validator: (text='') => {
+        if(text.split(/[\t\n\r ]/).length != 1)
+            return false;
+        return true;
+    },
+    execute:(bot, args) => {
+        console.log('creating explore goal')
+        bot.behaviors.goal.goals.goal.push(createGoal(bot, {type:'explore'}))
+        console.log('adding callback event!')
+        bot.behaviors.eventPool.addEvent('Команда', '"GUARD_USER" успешно выполнена')
+    }
+}
